7946dcb2006ae09fdc1a90036c9157b7cd14e6e7
Nicolas Elias <nicolaselias@pm.me>
2022-09-12 23:12:29 -0300

    site pronto falta por link

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './styles/input.css'
+import './styles/font.css'
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-4xl py-10 px-20 rounded my-40">
+      <a href="">
+        {props.title}
+      </a>
+    </button>
+  )
+}
+
+function App() {
+
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col items-center my-40">
+      <h1 className="text-center text-6xl font-extrabold text-white text-shadow">Marea-Turbo Hackerclub</h1>
+      <Button title="Chamar RUbot para Telegram"></Button>
+    </div>
+  )
+}
+
+export default App
